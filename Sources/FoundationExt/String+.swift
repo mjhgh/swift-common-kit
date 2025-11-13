caa91df8367ddfb4a029312bee3d625b12ae1b1f
mjh_gh <[email protected]>
2025-11-13 18:21:36 +0800

    Fix asDataUTF8 property to return optional Data and adjust decodingHTMLEntities method for better safety

--- Sources/FoundationExt/String+.swift
+++ Sources/FoundationExt/String+.swift
@@ -1,6 +1,9 @@
+#if canImport(AppKit)
+  import AppKit
+#endif
 extension String {
-  public var asDataUTF8: Data {
-    return data(using: .utf8)!
+  public var asDataUTF8: Data? {
+    return data(using: .utf8)
   }
   public var asURL: URL? {
     return URL(string: self)
@@ -18,4 +21,23 @@ extension String {
     }
     return self
   }
+  #if canImport(AppKit)
+    public func decodingHTMLEntities() -> String {
+      guard let data = self.data(using: .utf8) else {
+        return self
+      }
+      let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+        .documentType: NSAttributedString.DocumentType.html,
+        .characterEncoding: String.Encoding.utf8.rawValue,
+      ]
+      if let attributedString = try? NSAttributedString(
+        html: data, options: options, documentAttributes: nil)
+      {
+        return attributedString.string
+      } else {
+        return self
+      }
+
+    }
+  #endif
 }
